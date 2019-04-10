6fc9f652e8fd06237e1f37eadab28f5d7fc3c9cc
Camille Baillat <camille.baillat@pm.me>
2019-04-10 11:18:12 +0200

    Add communication through listener in fragment/activity
    - Add a FloatingActionButton in dog list layout
    - Add an listener interface in the DogListFragment
    - Attach the listener to the DogListFragment
    - Attach the callback to the floating button
    - Add the listener's callback implementation in the MainActivity
    
    If we click on the floating button, we can see that the fragment is able to communicate with the parent's activity.

--- app/src/main/java/me/camillebc/basics/view/MainActivity.kt
+++ app/src/main/java/me/camillebc/basics/view/MainActivity.kt
@@ -2,10 +2,16 @@ package me.camillebc.basics.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import me.camillebc.basics.R
 import me.camillebc.basics.view.fragment.DogListFragment
 
-class MainActivity : AppCompatActivity() {
+/**
+ * The parent activity must extend the fragment's listeners.
+ */
+class MainActivity :
+    AppCompatActivity(),
+    DogListFragment.OnAddClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,4 +22,19 @@ class MainActivity : AppCompatActivity() {
             .add(R.id.constraintLayout_main_fragmentContainer, dogListFragment)
             .commit()
     }
+
+    /**
+     * This is the implementation of the [DogListFragment.OnAddClickListener].
+     */
+    override fun onDogListAddClick() {
+        toast("Button clicked")
+    }
+
+}
+
+/**
+ * Small extension function for activities. It simply Toast a message
+ */
+fun AppCompatActivity.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
--- app/src/main/java/me/camillebc/basics/view/fragment/DogListFragment.kt
+++ app/src/main/java/me/camillebc/basics/view/fragment/DogListFragment.kt
@@ -1,19 +1,23 @@
 package me.camillebc.basics.view.fragment
 
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_dog_list.*
 
 import me.camillebc.basics.R
+import java.lang.RuntimeException
 
 /**
  * A simple [Fragment] subclass.
  *
  */
 class DogListFragment : Fragment() {
+    private var onAddClickListener: OnAddClickListener? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +25,53 @@ class DogListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_dog_list, container, false)
+    }
+
+    /**
+     * [onActivityCreated] is called after [onCreateView]. The button's view is already inflated.
+     * [onActivityCreated] is called after the parent's activity is fully functional.
+     *
+     * We can attach the callback safely here, as the activity is fully functional.
+     */
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        activity?.let {
+            button_dogList_add.setOnClickListener{ onAddClickListener?.onDogListAddClick() }
+        }
+    }
+
+    /**
+     * [onAttach] is called when the activity is created.
+     *
+     * WARNING: [onAttach] holds only a *reference* to the parent's activity. It does *not* mean that the parent's
+     * activity is fully functional.
+     *
+     * We can attach the listener safely, as we only need the reference to check that the parent's activity implements
+     * the listener.
+     * We CANNOT attach the listener's callback itself, as it the implementation uses [Toast] which relies on the
+     * activity to be fully functional.
+     */
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        // Attach the parent's activity listener to the fragment
+        if (context is OnAddClickListener)
+            onAddClickListener = context as OnAddClickListener
+        else
+            throw RuntimeException("$context must implement OnAddClickListener")
+    }
+
+    /**
+     * We detach the listener when the fragment is detached from the parent's activity
+     */
+    override fun onDetach() {
+        super.onDetach()
+        onAddClickListener = null
+    }
+    /**
+     * interface for the button click listener. The callback needs to be implemented in the parent's activity
+     */
+    interface OnAddClickListener {
+        fun onDogListAddClick()
     }
 
 }
